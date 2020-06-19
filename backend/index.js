988dea432f731a56484f5bce71b962b1f2d9e8e6
kecyGaurab <kecy.gaurab@gmail.com>
2020-06-19 15:45:38 +0300

    backend connected to Mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,34 @@
+/* eslint-disable no-console */
+/* eslint-disable no-const-assign */
 const express = require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
+require('dotenv').config();
 const cors = require('cors');
+const Note = require('./models/note');
 
 app.use(cors());
 
+app.use(bodyParser.json());
+
 app.use(express.static('build'));
 
-const notes = [
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(requestLogger);
+
+let notes = [
   {
     content: 'eat',
     done: false,
@@ -30,13 +51,54 @@ const notes = [
   },
 ];
 
-app.get('/notes', (req, res) => {
-  res.send('Hello world');
+app.get('api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((n) => n.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
 });
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes);
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then((data) => {
+    response.json(data.map((note) => note.toJSON()));
+  });
 });
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+// eslint-disable-next-line consistent-return
+app.post('/api/notes', (request, response) => {
+  const { body } = request;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing',
+    });
+  }
+
+  const note = {
+    content: body.content,
+    done: body.done || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+  response.json(note);
+});
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((n) => n.id !== id);
+  response.status(204).end();
+});
+
+app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
