d9ed11c6e4490140ced86a84334ec40c6d6553f5
kecyGaurab <kecy.gaurab@gmail.com>
2020-06-21 10:24:50 +0300

    uibuild

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const notesRouter = require('./controllers/notes');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
-
+const path = require('path')
 
 logger.info('connecting to', config.MONGODB_URI);
 // mongoose.set('useCreateIndex', true)
@@ -33,9 +33,9 @@ app.use(middleware.requestLogger);
 
 app.use('/api/notes', notesRouter);
 
-// app.use((req, res) => {
-// 	res.sendFile(path.join(__dirname, 'client/build/index.html'));
-// });
+app.use((req, res) => {
+	res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
