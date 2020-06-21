33466d29f5c5d8844bffe138105eab189ff068c7
kecyGaurab <kecy.gaurab@gmail.com>
2020-06-21 13:35:24 +0300

    uibuild

---
+++ build/precache-manifest.f6ed5096d877c13cd6305b8cd19a9b5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38abf608ee1da73edc60230348ecfe03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fa93d4e154247f91fc9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9c123b72489c5942346b",
+    "url": "/static/js/2.48921e1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.48921e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fa93d4e154247f91fc9",
+    "url": "/static/js/main.03744655.chunk.js"
+  },
+  {
+    "revision": "822741c3dcc2c5eee2c5",
+    "url": "/static/js/runtime-main.a17ee0f4.js"
+  }
+]);
